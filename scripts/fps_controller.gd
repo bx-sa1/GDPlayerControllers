957aaff1af333253157415dbe07300bd17927c8e
bx_sa1 <bms110403@gmail.com>
2025-11-10 01:23:04 +0000

    merge fps and tps

--- scripts/fps_controller.gd
+++ scripts/fps_controller.gd
@@ -5,10 +5,24 @@ class_name FPSController extends BaseController
 @export var SENSITIVITY = 1
 @export var GRAVITY = 10
 @export var JUMP_ACCEL = 5
+@export var CAM_DISTANCE = 10.0
+
+enum ViewState {
+	FirstPerson,
+	ThirdPerson
+	}
+@export var view_state: ViewState:
+	set(v):
+		view_state = v
+		view_state_change.emit(v)
+signal view_state_change
+
+var pitch = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
+	view_state_change.connect(on_view_state_change)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -38,5 +52,20 @@ func _input(event: InputEvent) -> void:
 
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * 0.1 * SENSITIVITY))
-		camera.rotate_x(deg_to_rad(-event.relative.y * 0.1 * SENSITIVITY))
-		camera.rotation.x = clampf(camera.rotation.x, -PI/2, PI/2)
+
+		if view_state == ViewState.FirstPerson:
+			camera.rotate_x(deg_to_rad(-event.relative.y * 0.1 * SENSITIVITY))
+			camera.rotation.x = clampf(camera.rotation.x, -PI/2, PI/2)
+		elif view_state == ViewState.ThirdPerson:
+			pitch += deg_to_rad(-event.relative.y * 0.1 * SENSITIVITY)
+			pitch = clampf(pitch, -PI/2, PI/2)
+			camera.position.x = cos(pitch) * CAM_DISTANCE
+			camera.position.y = sin(pitch) * CAM_DISTANCE
+			camera.position.z = cos(pitch) * CAM_DISTANCE
+			camera.look_at(position)
+
+func on_view_state_change(v: ViewState) -> void:
+	if v == ViewState.FirstPerson:
+		camera.position = Vector3.ZERO
+	elif v == ViewState.ThirdPerson:
+		camera.position.z -= CAM_DISTANCE
